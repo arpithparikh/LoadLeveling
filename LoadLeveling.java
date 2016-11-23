c89a360a125333186efe762cf107a827a5e137c7
Arpit Parikh <arpithparikh@gmail.com>
2016-11-23 09:45:08 -0500

    LoadLeveling Problem

---
+++ LoadLeveling.java
@@ -0,0 +1,38 @@
+
+public class LoadLeveling {
+    public static void main(String[] args) {
+
+        int m = 5;//Integer.parseInt(args[0]);
+        int n = 7;//Integer.parseInt(args[1]);
+        int s = 10;//Integer.parseInt(args[2]);
+
+        // Create server queues.
+        RandomQueue<Queue<Integer>> servers = new RandomQueue<Queue<Integer>>();
+
+        for (int i = 0; i < m; i++) {
+            servers.enqueue(new Queue<Integer>());
+        }
+        
+        // Assign an item to a server
+        for (int j = 0; j < n; j++) {
+
+            // Pick a random server, update if new min.
+            Queue<Integer> min = servers.sample();
+            for (int k = 1; k < s; k++) {
+                Queue<Integer> queue = servers.sample();
+                if (queue.size() < min.size()) min = queue;
+            }
+
+            // min is the shortest server queue
+            min.enqueue(j);
+        }
+
+        int i = 0;
+        double[] lengths = new double[m];
+        for (Queue<Integer> queue : servers) {
+            lengths[i++] = queue.size();
+            StdDraw.setYscale(0, 2.0*n/m);
+            StdStats.plotBars(lengths);
+        }
+    }
+}
